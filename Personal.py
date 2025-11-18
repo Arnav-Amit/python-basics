f2a01b38c08e7cfa76dee20b78ed68ee4a0337a0
Arnav-Amit <amitkumartrss54@gmail.com>
2025-11-18 20:29:34 +0530

    first commit

---
+++ Personal.py
@@ -0,0 +1,13 @@
+# Build a Personal Information Manager that stores and displays your name, age, city, and hobbies with formatted output
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city:")
+hobbies = input("your hobbies : ")
+
+print(f"your: {name}")
+print(f"your: {age}")
+print(f"city : {city}")
+print(f"hobiies: {hobbies}")
+
+
+
